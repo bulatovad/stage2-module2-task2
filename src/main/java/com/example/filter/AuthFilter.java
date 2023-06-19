af95d3d16f51d2611c2068432584656dde9ef0ae
bulatovad <damira.bulatova@gmail.com>
2023-06-19 23:13:00 +0600

    filter bugfix

--- src/main/java/com/example/filter/AuthFilter.java
+++ src/main/java/com/example/filter/AuthFilter.java
@@ -7,7 +7,10 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
-@WebFilter("/AuthFilter")
+@WebFilter(
+        filterName = "AuthFilter",
+        urlPatterns = {"/user/*"},
+        servletNames = {"LoginServlet", "LogoutServlet"})
 public class AuthFilter implements Filter {
     //write your code here!
     @Override
@@ -15,12 +18,14 @@ public class AuthFilter implements Filter {
         HttpServletRequest req = (HttpServletRequest) servletRequest;
         HttpServletResponse res = (HttpServletResponse) servletResponse;
 
-        String uri = req.getRequestURI();
         HttpSession session = req.getSession(false);
 
-        if(session == null && !(uri.contains("/user/*"))){
-            res.sendRedirect("login.jsp");
-        }else{
+
+        if(session == null){
+            req.getRequestDispatcher("login.jsp").forward(req, res);
+        } else if(session.getAttribute("user")==null) {
+            req.getRequestDispatcher("login.jsp").forward(req, res);
+        } else {
             // pass the request along the filter chain
             filterChain.doFilter(req, res);
         }
